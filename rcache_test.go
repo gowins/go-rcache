775b1be971212f04bc534694a1cbbbc97024361e
wuyx <wuyx@weipaitang.com>
2020-03-09 20:59:50 +0800

    增加并发控制，并增加定时刷新功能

---
+++ rcache_test.go
@@ -0,0 +1,66 @@
+package rcache
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/micro/go-micro/registry"
+	"github.com/micro/go-plugins/registry/etcdv3"
+)
+
+func Test_cache_updateByTimer(t *testing.T) {
+	type args struct {
+		service string
+	}
+	tests := []struct {
+		name   string
+		args   args
+	}{
+		{
+			name: "no cache",
+			args:args{
+				service: "wpt.api.api",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := New(etcdv3.NewRegistry(registry.Addrs("localhost:2379")), WithTTL(time.Minute)).(*cache)
+			go func() {
+				time.AfterFunc(time.Second, c.Stop)
+			}()
+
+			// c.refreshByTimer(tt.args.service)
+			fmt.Printf("%+v", c.cache)
+			fmt.Printf("%+v", c.ttls)
+		})
+	}
+}
+
+func TestCache_getFromRegistry(t *testing.T) {
+	var m sync.Map
+	type s struct {
+		p *int
+	}
+
+	for i :=0; i < 1000; i++ {
+		go func(i int) {
+			 ss := new(sync.Mutex)
+			v, _ := m.LoadOrStore(i, ss)
+			ss.Lock()
+			val := v.(*sync.Mutex)
+			_ =val
+			// val.Lock()
+
+			println("1111")
+		}(i)
+	}
+
+	time.Sleep(time.Hour)
+}
+
+func BenchmarkCache_getFromRegistry(b *testing.B) {
+
+}
